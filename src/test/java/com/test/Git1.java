f080beff21a6309d4011a76ab58f4bdf68f7b529
Sivaprakash17 <sivaprakash170997@outlook.com>
2022-08-24 12:41:10 +0530

    First Push

---
+++ src/test/java/com/test/Git1.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Git1 {
+	
+	private void day1() {
+		System.out.println("DAY 1 CLASS");
+
+	}
+	private void day2() {
+		System.out.println("DAY 2 CLASS");
+
+	}
+	private void day3() {
+		System.out.println("DAY 3 CLASS");
+
+	}
+
+}
